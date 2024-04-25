fa97b7ee5a6b2020c2e70444173635b33ccf6fbe
truesamsepiol <true.samsepiol@gmail.com>
2024-04-25 11:49:03 +0200

    the structure of heart of script is ready

--- generate_trace/generate_trace.c
+++ generate_trace/generate_trace.c
@@ -3,10 +3,13 @@
 #include <unistd.h>  
 #include <string.h>
 
+
+#define SIZE_MAX_APP_NAME 256
+
 struct parameter{
 	char *path;
 	char *low;
-} setting = {.low="zipfian", .path="no_file"};
+} setting = {.low="zipf", .path="no_file"};
 
 static void usage() {                                                                                    
     fprintf(stderr,
@@ -42,17 +45,40 @@ static void check_params(int argc, char **argv) {
     }
 }
 
+void generate_file(){
+    FILE *desp  = fopen(setting.path, "r");
+    FILE *trace = fopen("traces", "w");
+
+    char app[SIZE_MAX_APP_NAME];
+
+    if(desp == NULL){
+        fprintf(stderr, "\n!!!    NO SUCH FILE    !!!\n");
+        exit(0);
+    }
+    if(trace == NULL){
+	fprintf(stderr, "\n!!!   ERROR to Open traces file   !!!\n");
+	exit(0);
+    }
+    
+    fprintf(trace, "app name,number of dpus,number of taskelt,input data\n");
+    while(fscanf(desp, "%s", app) == 1){
+        fprintf(trace, "%s\n", app);
+    }
+    
+    fclose(desp);
+    fclose(trace);
+}
+
+
 int main(int argc, char **argv){
     
     check_params(argc, argv);
-
     if(strcmp(setting.path, "no_file") == 0){
 	    fprintf(stderr, "\n---> Please specify path to application list <---\n");
 	    usage();
 	    exit(0);
     }
 
-
-    printf("All rigth\n");
+    generate_file(); 
     return 0;
 }
