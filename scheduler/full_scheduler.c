068e59c041abac41dc678bf1fa422269900df043
truesamsepiol <true.samsepiol@gmail.com>
2024-05-07 19:04:06 +0200

    full and single beta version are ready. v1.1

--- scheduler/full_scheduler.c
+++ scheduler/full_scheduler.c
@@ -10,7 +10,7 @@
 #define MAX_TRACES 10000
 #define NR_PARAMETERS 7
 #define SIZE_OF_COMMAND_PARAMETERS 30
-#define NR_DPUS_MAX 480
+#define NR_DPUS_MAX 13
 
 struct parameter{
 	char *path;
@@ -28,6 +28,8 @@ static void usage() {
 	"\n    -h        help"
         "\n");                                                                                           
 }
+
+int tour            = 0;
 
 static void check_params(int argc, char **argv) {                                                      
     
@@ -49,9 +51,7 @@ static void check_params(int argc, char **argv) {
     }
 }
 
-void full_scheduler(){
-
-	printf("\n\n################# start full scheduler #################\n\n");
+void parse_argv(){
 	
 	FILE *desp = fopen(setting.path, "r");
 	if(desp == NULL){
@@ -59,11 +59,7 @@ void full_scheduler(){
 		exit(0);
 	}
 	
-	int round           = 0;
-	int nr_dpus_total   = 0;
 	char buffer[MAX_TRACES];
-
-	struct dpu_set_t set, dpu;
 
 	while(fgets(buffer, MAX_TRACES, desp)){
 		char app_name[MAX_TRACES];
@@ -72,54 +68,80 @@ void full_scheduler(){
 		     data[MAX_TRACES];
 		sscanf(buffer, "%s %s %s %s", app_name, nr_dpus, nr_tasklets, data);
 
-		programs[round].program[0] = malloc(sizeof(char) * strlen(app_name));
-		strcpy(programs[round].program[0], app_name);
+		programs[tour].program[0] = malloc(sizeof(char) * strlen(app_name));
+		strcpy(programs[tour].program[0], app_name);
 		
-		programs[round].program[1] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[1], "-NR_DPUS");
+		programs[tour].program[1] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[1], "-NR_DPUS");
 
-		programs[round].program[2] = malloc(sizeof(char) * strlen(nr_dpus));
-		strcpy(programs[round].program[2], nr_dpus);
-		nr_dpus_total += atoi(nr_dpus);
+		programs[tour].program[2] = malloc(sizeof(char) * strlen(nr_dpus));
+		strcpy(programs[tour].program[2], nr_dpus);
 
-		programs[round].program[3] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[3], "-NR_TASKLETS");
+		programs[tour].program[3] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[3], "-NR_TASKLETS");
 
-		programs[round].program[4] = malloc(sizeof(char) * strlen(nr_tasklets));
-		strcpy(programs[round].program[4], nr_tasklets);
+		programs[tour].program[4] = malloc(sizeof(char) * strlen(nr_tasklets));
+		strcpy(programs[tour].program[4], nr_tasklets);
 
-		programs[round].program[5] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[5], "-d");
+		programs[tour].program[5] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[5], "-d");
 
-		programs[round].program[6] = malloc(sizeof(char) * strlen(data));
-		strcpy(programs[round].program[6], data);
+		programs[tour].program[6] = malloc(sizeof(char) * strlen(data));
+		strcpy(programs[tour].program[6], data);
 		
-		round += 1;
-	}	
+		tour += 1;
+	}
+}
 
-	DPU_ASSERT(dpu_alloc(nr_dpus_total, NULL, &set));
-	
-	int i = 0;
-	int sum_size = 0;
-	int nr_dpus  = atoi(programs[i].program[2]);
-	DPU_FOREACH(set, dpu){
-		sum_size += 1;
-		if(sum_size > nr_dpus){
-			i += 1;
-			nr_dpus += atoi(programs[i].program[2]);
+void full_scheduler(){
+
+	printf("\n\n################# start full scheduler #################\n\n");
+
+	parse_argv();
+
+	int i               = 0;
+	int j               = 0;
+	while(j < tour){
+		int nr_dpus_total = 0;
+		while(j < tour){
+			if(atoi(programs[j].program[2]) > NR_DPUS_MAX){
+					fprintf(stderr, "The program %s ask more than DPUS we have\n"\
+							"We have %d DPUS program ask %s DPUS \n\n", programs[j].program[0], \
+						       NR_DPUS_MAX, programs[j].program[2]);	
+					exit(0);
+			}
+			if(nr_dpus_total + atoi(programs[j].program[2]) <= NR_DPUS_MAX)
+				nr_dpus_total += atoi(programs[j].program[2]);
+			else
+				break;
+			j++;
 		}
-		DPU_ASSERT(dpu_load(dpu, programs[i].program[0], NULL));
+		
+		struct dpu_set_t set, dpu;
+		DPU_ASSERT(dpu_alloc(nr_dpus_total, NULL, &set));
+	
+		int sum_size = 0;
+		int nr_dpus  = atoi(programs[i].program[2]);
+		DPU_FOREACH(set, dpu){
+			sum_size += 1;
+			if(sum_size > nr_dpus){
+				i += 1;
+				nr_dpus += atoi(programs[i].program[2]);
+			}
+			DPU_ASSERT(dpu_load(dpu, programs[i].program[0], NULL));
+		}
+
+		DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
+
+		DPU_FOREACH(set, dpu)
+			DPU_ASSERT(dpu_log_read(dpu, stdout));
+	
+	
+		DPU_ASSERT(dpu_free(set));
+		i += 1;
 	}
 
-	DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
-
-	DPU_FOREACH(set, dpu)
-		DPU_ASSERT(dpu_log_read(dpu, stdout));
-	
-	
-	DPU_ASSERT(dpu_free(set));
-
-	printf("\n\n################# END Excution of %d programs on %d DPUS #################\n\n", round, nr_dpus);
+	printf("\n\n################# END Excution of %d programs #################\n\n", tour);
 }
 
 int main(int argc, char **argv){
--- scheduler/single_scheduler.c
+++ scheduler/single_scheduler.c
@@ -24,6 +24,8 @@ struct p{
 	char *program[NR_PARAMETERS];
 }programs[MAX_TRACES];
 
+int tour = 0;
+
 static void usage() {                                                                                    
     fprintf(stderr,
         "\nUsage:  ./program [options]\n"
@@ -53,8 +55,7 @@ static void check_params(int argc, char **argv) {
     }
 }
 
-void single_scheduler(){
-	printf("\n\n################# start single scheduler #################\n\n");
+void parse_argv(){
 	
 	FILE *desp = fopen(setting.path, "r");
 	if(desp == NULL){
@@ -62,8 +63,6 @@ void single_scheduler(){
 		exit(0);
 	}
 	
-	int round           = 0;
-	int nr_dpus_total   = 0;
 	char buffer[MAX_TRACES];
 
 	while(fgets(buffer, MAX_TRACES, desp)){
@@ -73,35 +72,46 @@ void single_scheduler(){
 		     data[MAX_TRACES];
 		sscanf(buffer, "%s %s %s %s", app_name, nr_dpus, nr_tasklets, data);
 
-		programs[round].program[0] = malloc(sizeof(char) * strlen(app_name));
-		strcpy(programs[round].program[0], app_name);
+		programs[tour].program[0] = malloc(sizeof(char) * strlen(app_name));
+		strcpy(programs[tour].program[0], app_name);
 		
-		programs[round].program[1] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[1], "-NR_DPUS");
+		programs[tour].program[1] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[1], "-NR_DPUS");
 
-		programs[round].program[2] = malloc(sizeof(char) * strlen(nr_dpus));
-		strcpy(programs[round].program[2], nr_dpus);
-		nr_dpus_total += atoi(nr_dpus);
+		programs[tour].program[2] = malloc(sizeof(char) * strlen(nr_dpus));
+		strcpy(programs[tour].program[2], nr_dpus);
 
-		programs[round].program[3] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[3], "-NR_TASKLETS");
+		programs[tour].program[3] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[3], "-NR_TASKLETS");
 
-		programs[round].program[4] = malloc(sizeof(char) * strlen(nr_tasklets));
-		strcpy(programs[round].program[4], nr_tasklets);
+		programs[tour].program[4] = malloc(sizeof(char) * strlen(nr_tasklets));
+		strcpy(programs[tour].program[4], nr_tasklets);
 
-		programs[round].program[5] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
-		strcpy(programs[round].program[5], "-d");
+		programs[tour].program[5] = malloc(sizeof(char) * SIZE_OF_COMMAND_PARAMETERS);
+		strcpy(programs[tour].program[5], "-d");
 
-		programs[round].program[6] = malloc(sizeof(char) * strlen(data));
-		strcpy(programs[round].program[6], data);
+		programs[tour].program[6] = malloc(sizeof(char) * strlen(data));
+		strcpy(programs[tour].program[6], data);
 		
-		round += 1;
+		tour += 1;
 	}	
 
 
-	for(int id = 0; id < round; id++){
-		struct dpu_set_t set, dpu;
+}
 
+void single_scheduler(){
+	printf("\n\n################# start single scheduler #################\n\n");
+
+	parse_argv();
+	
+	for(int id = 0; id < tour; id++){
+		struct dpu_set_t set, dpu;
+		if(atoi(programs[id].program[2]) > NR_DPUS_MAX){
+			fprintf(stderr, "The program %s ask more than DPUS we have\n"\
+				"We have %d DPUS program ask %s DPUS \n\n", programs[id].program[0], \
+	 			NR_DPUS_MAX, programs[id].program[2]);	
+			exit(0);
+		}
 		DPU_ASSERT(dpu_alloc(atoi(programs[id].program[2]), NULL, &set));
 		DPU_ASSERT(dpu_load(set, programs[id].program[0], NULL));
 		DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
@@ -111,7 +121,7 @@ void single_scheduler(){
 		DPU_ASSERT(dpu_free(set));
 	}
 
-	printf("\n\n################# END Excution of %d programs #################\n\n", round);
+	printf("\n\n################# END Excution of %d programs #################\n\n", tour);
 }
 
 int main(int argc, char **argv){
